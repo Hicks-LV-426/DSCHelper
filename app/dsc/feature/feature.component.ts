b7d2ef16197c35b0c3b6d4bc5a1703862e499e2c
Aidas Pasilis <ai-des@hotmail.com>
2017-02-02 00:17:39 +0200

    feature list now reports about selected/unselected features

--- app/dsc/feature/feature.component.ts
+++ app/dsc/feature/feature.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 import { FeatureService } from './feature.service';
+import { DependencyCoordinator } from './dependency.coordinator';
 import { Server } from './server';
 import { Feature } from './feature';
  
 @Component({
   selector: 'dsc-feature',
   templateUrl: 'app/dsc/feature/feature.component.html',
-  styleUrls: ['app/dsc/feature/feature.component.css']
+  styleUrls: ['app/dsc/feature/feature.component.css'],
+  providers: [DependencyCoordinator]
 })
 export class FeatureComponent implements OnInit 
 {
@@ -15,12 +17,23 @@ export class FeatureComponent implements OnInit
   servers: Server[];
 
 
-  constructor(private _featureService : FeatureService ){}
+  constructor(private _featureService: FeatureService, private _coordinator: DependencyCoordinator) { }
 	ngOnInit()
   {
     var promise = this._featureService.getServers().then((servers) =>
     {
       this.servers = servers;
+    });
+
+    // subscribe to feature selected and unselected events
+    this._coordinator.itemAdded.subscribe((value) =>
+    {
+      console.info(value + " added")
+      //console.info(arr1.concat(arr2));
+    });
+    this._coordinator.itemRemoved.subscribe((value) => {
+      console.info(value + " removed")
+      //console.info(arr1.concat(arr2));
     });
   }
   getFeatures(): Feature[]
--- app/dsc/feature/feature.list.component.ts
+++ app/dsc/feature/feature.list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+import { DependencyCoordinator } from './dependency.coordinator';
 import { Feature } from './feature';
 
 @Component({
@@ -12,7 +13,9 @@ export class FeatureListComponent implements OnInit
   @Input() features: Feature[];
   @Input() parent: string;
 
-  constructor() { }
+  selectedFeatures: string[] = [];
+
+  constructor(private _coordinator: DependencyCoordinator) { }
   ngOnInit() { }
 
   getFeatures(): Feature[]
@@ -20,5 +23,22 @@ export class FeatureListComponent implements OnInit
     if (this.features === undefined) return [];
 
     return this.features.filter(f => f.Parent === this.parent).sort((n1, n2) => n1.Name > n2.Name ? 1 : 0);
+  }
+  isSelected(name: string): boolean
+  {
+    return this.selectedFeatures.find(f => f === name) != undefined;
+  }
+  itemToggle(name: string)
+  {
+    if (this.isSelected(name))
+    {
+      this.selectedFeatures.splice(this.selectedFeatures.indexOf(name), 1);
+      this._coordinator.remove(name);
+    }
+    else
+    {
+      this.selectedFeatures.push(name);
+      this._coordinator.add(name);
+    }
   }
 }
